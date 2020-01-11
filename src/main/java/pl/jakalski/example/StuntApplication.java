69bf6af05bd13839c265e9628e08f96010455678
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2020-01-11 13:37:37 +0100

    init version

---
+++ src/main/java/pl/jakalski/example/StuntApplication.java
@@ -0,0 +1,13 @@
+package pl.jakalski.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jakalski/example/StuntApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakalski.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
