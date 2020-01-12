bcfbe27f6e6f01bd066b0a8b0155950393f5560f
qbunjo <jakub@jakalski.pl>
2020-01-12 13:46:08 +0100

    init class

---
+++ src/main/java/pl/jakalski/example/Meeting.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+
+public class Meeting {
+ private String name;
+ private Integer day;
+ private Integer montj;
+ private Integer year;
+ private Integer hour;
+ private Integer minute;
+ private List <WhosInvited> getList() {
+		return list;
+	}
+ 
+}
