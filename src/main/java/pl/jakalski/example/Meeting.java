b3609954ee9d8102e8e4eb3237b87c4ccb41f9c2
qbunjo <jakub@jakalski.pl>
2020-01-12 13:58:28 +0100

    repair

--- src/main/java/pl/jakalski/example/Meeting.java
+++ src/main/java/pl/jakalski/example/Meeting.java
@@ -2,14 +2,15 @@ import java.util.List;
 
 
 public class Meeting {
- private String name;
- private Integer day;
- private Integer montj;
- private Integer year;
- private Integer hour;
- private Integer minute;
- private List <WhosInvited> getList() {
-		return list;
-	}
- 
+    private String name;
+    private Integer day;
+    private Integer month;
+    private Integer year;
+    private Integer hour;
+    private Integer minute;
+
+    private List<WhosInvited> getList() {
+        return list;
+    }
+
 }
