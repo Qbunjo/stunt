041f91fef702b5b2f23c7cef5835398133c045b5
Qbunjo <jakub@jakalski.pl>
2020-01-14 18:34:06 +0100

    meeting creator added

--- src/main/java/pl/jakalski/stunt/meetings/MeetingManager.java
+++ src/main/java/pl/jakalski/stunt/meetings/MeetingManager.java
@@ -5,11 +5,11 @@ import java.time.LocalDateTime;
 public class MeetingManager {
     private Object LocalDateTime;
 
-    public Meeting(String identifier) {
-        private List<MeetingList> meetingList;
-        private String name;
-        private String place;
-        private LocalDateTime date;
+    public void Meeting(String identifier) {
+        List<MeetingList> meetingList;
+        String name;
+        String place;
+        LocalDateTime date;
 
         public void AddMeeting (String name, LocalDateTime date, String place){
             MeetingList newMeeting = new MeetingList(String name, LocalDateTime date, String place);
