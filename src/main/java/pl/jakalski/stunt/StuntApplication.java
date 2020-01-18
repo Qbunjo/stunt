360fee0af4a6edf940e17829b0ca6bc992e0729b
Wiiitek <kubiczak.jakub@gmail.com>
2020-01-18 18:24:54 +0100

    Spring security added

--- src/main/java/pl/jakalski/stunt/StuntApplication.java
+++ src/main/java/pl/jakalski/stunt/StuntApplication.java
@@ -2,12 +2,14 @@ package pl.jakalski.stunt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class StuntApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StuntApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StuntApplication.class, args);
+    }
 
 }
